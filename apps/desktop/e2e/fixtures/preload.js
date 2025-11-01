5fd5aa8e0662470e73f0eacbfaa526eec6ac8f45
yeomin4242 <whereismysejong@naver.com>
2025-11-01 17:50:17 +0900

    style: unify quote style in preload.js and add calibration request channel

--- apps/desktop/e2e/fixtures/preload.js
+++ apps/desktop/e2e/fixtures/preload.js
@@ -1,10 +1,11 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
 const CHANNELS = new Set([
-  'system:ping',
-  'worker:request',
-  'worker:status',
-  'worker:response',
+  "system:ping",
+  "worker:request",
+  "worker:status",
+  "worker:response",
+  "calibration:request",
 ]);
 
 const ensureAllowed = (channel) => {
@@ -29,7 +30,11 @@ const api = {
       ensureAllowed(channel);
       ipcRenderer.once(channel, (_event, ...args) => callback(...args));
     },
+    invoke(channel, ...args) {
+      ensureAllowed(channel);
+      return ipcRenderer.invoke(channel, ...args);
+    },
   },
 };
 
-contextBridge.exposeInMainWorld('electron', api);
+contextBridge.exposeInMainWorld("electron", api);
