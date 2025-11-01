05b745ba4af55c21174e66ffc2796380a728a083
Eddie Sohn <junyoung.eddie@gmail.com>
2025-11-02 00:36:29 +0900

    feat(webpack): include shared environment variables in preload development config

--- apps/desktop/.erb/configs/webpack.config.preload.dev.ts
+++ apps/desktop/.erb/configs/webpack.config.preload.dev.ts
@@ -3,7 +3,7 @@ import webpack from "webpack";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { merge } from "webpack-merge";
 import checkNodeEnv from "../scripts/check-node-env";
-import baseConfig from "./webpack.config.base";
+import baseConfig, { SHARED_ENV_VARS } from "./webpack.config.base";
 import webpackPaths from "./webpack.paths";
 
 // When an ESLint server is running, we can't set the NODE_ENV so we'll check if it's
@@ -48,6 +48,7 @@ const configuration: webpack.Configuration = {
      */
     new webpack.EnvironmentPlugin({
       NODE_ENV: "development",
+      ...SHARED_ENV_VARS,
     }),
 
     new webpack.LoaderOptionsPlugin({
