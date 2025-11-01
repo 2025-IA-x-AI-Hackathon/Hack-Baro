cb7e273a9749069d7d4ac340abd1d83c4f91ecfd
yeomin4242 <whereismysejong@naver.com>
2025-11-01 17:44:15 +0900

    style: standardize quotes and formatting in webpack configuration

--- apps/desktop/.erb/configs/webpack.config.base.ts
+++ apps/desktop/.erb/configs/webpack.config.base.ts
@@ -1,30 +1,28 @@
 /**
  * Base webpack config used across other specific configs
  */
-
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-unresolved */
-
-import path from 'path';
-import webpack from 'webpack';
-import TsconfigPathsPlugins from 'tsconfig-paths-webpack-plugin';
-import webpackPaths from './webpack.paths';
+import path from "path";
+import TsconfigPathsPlugins from "tsconfig-paths-webpack-plugin";
+import webpack from "webpack";
+import webpackPaths from "./webpack.paths";
 
 // eslint-disable-next-line global-require
-const externals = require('../../release/app/package.json').dependencies || {};
+const externals = require("../../release/app/package.json").dependencies || {};
 
 const workspacePackagesPath = path.resolve(
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   webpackPaths.rootPath,
-  '..',
-  '..',
-  'packages',
+  "..",
+  "..",
+  "packages",
 );
 
 const configuration: webpack.Configuration = {
   externals: [...Object.keys(externals as Record<string, string>)],
 
-  stats: 'errors-only',
+  stats: "errors-only",
 
   module: {
     rules: [
@@ -43,14 +41,14 @@ const configuration: webpack.Configuration = {
           return isNodeModule && !isBaroWorkspaceModule;
         },
         use: {
-          loader: 'ts-loader',
+          loader: "ts-loader",
           options: {
             // Remove this line to enable type checking in webpack builds
             transpileOnly: true,
             allowTsInNodeModules: true,
             compilerOptions: {
-              module: 'esnext',
-              moduleResolution: 'bundler',
+              module: "esnext",
+              moduleResolution: "bundler",
             },
           },
         },
@@ -61,25 +59,25 @@ const configuration: webpack.Configuration = {
   output: {
     path: webpackPaths.srcPath,
     // https://github.com/webpack/webpack/issues/1114
-    library: { type: 'commonjs2' },
+    library: { type: "commonjs2" },
   },
 
   /**
    * Determine the array of extensions that should be used to resolve modules.
    */
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-    modules: [webpackPaths.srcPath, workspacePackagesPath, 'node_modules'],
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    modules: [webpackPaths.srcPath, workspacePackagesPath, "node_modules"],
     extensionAlias: {
-      '.js': ['.ts', '.tsx', '.js'],
-      '.cjs': ['.cts', '.cjs'],
-      '.mjs': ['.mts', '.mjs'],
+      ".js": [".ts", ".tsx", ".js"],
+      ".cjs": [".cts", ".cjs"],
+      ".mjs": [".mts", ".mjs"],
     },
     // There is no need to add aliases here, the paths in tsconfig get mirrored
     plugins: [new TsconfigPathsPlugins()],
   },
 
-  plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: 'production' })],
+  plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: "production" })],
 };
 
 export default configuration;
